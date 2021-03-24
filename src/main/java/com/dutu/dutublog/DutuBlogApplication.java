b28b548f266fa3b1ae808c82a78a529e8bc20ee3
dutu-dhn <793665938@qq.com>
2021-03-24 09:33:28 +0800

    前端页面

---
+++ src/main/java/com/dutu/dutublog/DutuBlogApplication.java
@@ -0,0 +1,13 @@
+package com.dutu.dutublog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutuBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutuBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dutu/dutublog/DutuBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutu.dutublog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutuBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
